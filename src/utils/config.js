515b22b739a79ad069c807ba0b0b8d84875913e7
honeybeast <babystar.shine2019@gmail.com>
2021-08-10 16:47:20 +0900

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,7 @@
+export default {
+  siteTitle: 'Tania Rascia',
+  siteUrl: 'https://www.taniarascia.com',
+  siteLogo: '/logo.png',
+  description:
+    'Software engineer and open source creator. This is my digital garden.',
+}
